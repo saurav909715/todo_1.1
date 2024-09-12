7c5a85c9d0c0ada55340ef3f5cfcda16c5361feb
saurav909715 <sauravkumarobra51@gmail.com>
2024-09-12 18:15:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Use a single CORS configuration to handle multiple origins
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://192.168.99.1:5173','https://todo-1-1-frontend.vercel.app/'], // Allow multiple origins
+  origin: ['http://localhost:5173', 'http://192.168.99.1:5173','https://todo-1-1-frontend.vercel.app'], // Allow multiple origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Define allowed methods
   credentials: true, // Allow credentials like cookies
 }));
